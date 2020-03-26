b9522debbf1524e7a206bfecd2a5b36cf5edf33e
Dong Don <don.dong@volvo.com>
2020-03-26 17:27:38 +0800

    [Provide global variables]

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var dmixin =require('./js/Dmixin').dmixin
-var init = function(Vue){
+import {dmixin}  from './js/Dmixin'
+var superVue = function(Vue){
     Vue.prototype.$listen = new Vue()
     Vue.mixin(dmixin)
     var  superVue = {}
@@ -11,5 +11,13 @@ var init = function(Vue){
         superVue.callMethod(call.superRef,call.method,call.data)
     }
     Vue.prototype.$superVue =superVue
+    return  superVue;
 }
-export {init};
+
+
+// 为组件提供 install 安装方法，供按需引入
+superVue.install = function (Vue) {
+    superVue.init(Vue)
+}
+
+export {superVue};
