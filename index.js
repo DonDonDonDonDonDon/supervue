e6a2bc802508728e94e5702ac31b1331bd511996
Dong Don <don.dong@volvo.com>
2020-03-31 11:46:00 +0800

    []

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 import {dmixin}  from './js/Dmixin'
-var superVue = function(Vue){
-    Vue.prototype.$listen = new Vue()
-    Vue.mixin(dmixin)
-    var  superVue = {}
-    superVue.callMethod=function(superRef,method,data){
-        const event = superRef+"."+method;
-        Vue.prototype.$listen.$emit(event, data);
-    }
-    superVue.call=function(call){
-        superVue.callMethod(call.superRef,call.method,call.data)
-    }
-    Vue.prototype.$superVue =superVue
-    return  superVue;
-}
+const superVue = {
+    init : function(Vue){
+        Vue.prototype.$listen = new Vue()
+        try {
+            Vue.mixin(dmixin)
 
+        }catch (e) {
+
+        }
+        var  superVue = {}
+        superVue.callMethod=function(superRef,method,data){
+            const event = superRef+"."+method;
+            Vue.prototype.$listen.$emit(event, data);
+        }
+        superVue.call=function(call){
+            superVue.callMethod(call.superRef,call.method,call.data)
+        }
+        console.log("$$$$$$$$$$$$$$$################@@@@@@@@@@@@@@@@@@@@@@")
+        Vue.prototype.$superVue =superVue
+
+        return  superVue;
+    }
+}
 
 // 为组件提供 install 安装方法，供按需引入
 superVue.install = function (Vue) {
